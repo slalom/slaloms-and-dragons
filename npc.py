404d2633081e1005e1e6eb3af1a0ceb551d2e931
james.meadow <james.meadow@slalom.com>
2019-03-20 18:22:56 -0700

    begin npc class

---
+++ npc.py
@@ -0,0 +1,17 @@
+
+
+
+class npc: 
+    """
+    creates a class of non-player characters 
+
+    """ 
+
+
+    def __init__:(self, name, npc_class): 
+        self.name = name 
+        self.npc_class = npc_class 
+
+
+
+
