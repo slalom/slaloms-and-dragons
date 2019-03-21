09052d44a3c78cba57984a43b168ea2e35807c64
Dan Siwiec <daniel.siwiec@gmail.com>
2019-03-20 21:24:07 -0700

    Add battle randomization

--- engine/step_handler.py
+++ engine/step_handler.py
@@ -1,5 +1,12 @@
+import random
+
 def fight(character, monster):
-  print('Fight!')
+  randomint = random.randint(1,6)
+  monster_name = monster.get('name')
+  if monster.get('strength') > randomint:
+    print(f'You were defeated by {monster_name}')
+  else:
+    print(f'You defeated {monster_name}')
 
 def pickup(character, trophy):
   print('You found something!')
