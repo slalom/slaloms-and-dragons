5553268f04f4a22b1cb5c0e364267e803e4d4e53
Amarpreet Singh <amarpreet.singh1985@gmail.com>
2019-03-27 15:14:22 -0700

    add colored module to add colors

--- game/__main__.py
+++ game/__main__.py
@@ -3,9 +3,19 @@ import game.game as game
 import emoji
 import sys
 import time
+import pyfiglet
+from colored import fg, bg, attr
 
 
-animation = emoji.emojize("Let's play SLALOM & DRAGONS :dragon:")
+color = fg('orchid')
+reset = attr('reset')
+bold = attr('bold')
+result = pyfiglet.figlet_format("SLALOM & DRAGONS")
+print(color + result + reset)
+
+animation = emoji.emojize(
+    "%s%sLet's play SLALOM & DRAGONS%s :dragon:" %
+    (color, bold, reset))
 
 for i in animation:
     time.sleep(0.1)
