dc2455ba997a921fe3262fc1164585d7d371a549
Dan Siwiec <daniel.siwiec@gmail.com>
2019-03-28 22:14:53 -0700

    Extract string values

--- game/animations.py
+++ game/animations.py
@@ -4,18 +4,20 @@ import time
 import pyfiglet
 from colorama import Fore, Back, Style
 
+title = 'slaloms & dragons'
+subtitle = "Let's play SLALOMS & DRAGONS :dragon:"
+
 def show_intro():
   color = Fore.MAGENTA
   font_bold = Style.BRIGHT
-  result = pyfiglet.figlet_format("S L A L O M S  &  D R A G O N S", font="mini")
+  result = pyfiglet.figlet_format(' '.join(list(title.upper())), font='mini')
   print(color + font_bold + result)
 
-  animation = emoji.emojize(color + font_bold +
-                            "Let's play SLALOMS & DRAGONS :dragon:")
+  animation = emoji.emojize(color + font_bold + subtitle)
 
   for i in animation:
       time.sleep(0.1)
       sys.stdout.write(i)
       sys.stdout.flush()
-  print("\n")
+  print('\n')
   print(Style.RESET_ALL)
