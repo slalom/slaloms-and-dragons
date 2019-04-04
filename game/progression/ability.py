671bbafa09d72fc6a3262b1aa3c69f0d8e3266fd
Amarpreet Singh <amarpreet.singh1985@gmail.com>
2019-04-03 22:58:32 -0700

    assign random powers to characters

--- game/progression/ability.py
+++ game/progression/ability.py
@@ -1,23 +1,10 @@
-from .power import Power
-
-
-powers = []
-special_power = ""
+import game.progression.power as Power
 
 
 def get_ability(character):
     print('\n')
     print('Lets see your powers', character.name, ":")
-    get_powers(character)
-
-
-def get_powers(character):
-
-    if character.race == "Human":
-        powers = ["Sword", "Knife"]
-        special_power = "I can speak to dragons"
-        print('You can fight well with: ')
-        print(powers[0], '⚔️')
-        print(powers[1], '🗡️')
-        print(special_power, '🐉')
-        print()
+    power = Power.power()
+    for power in power.GetPower():
+        print(power)
+    print()
--- game/progression/power.py
+++ game/progression/power.py
@@ -1,22 +1,23 @@
 import random
 
+# Dictionary with powers and descriptions
+powers = {
+    "Sword": "Heimdall let me borrow his Sword",
+    "Knife": "I have lots of sharp knives",
+    "Flight": "Ever Seen Neo Fly. Yeah I can do that",
+    "ExtraStrength": "Think Hulk stength but no gamma radiation",
+    "Invisibility": "I’ve mastered the ability of standing so incredibly still that I become invisible to the eye. Watch. My movement’s so slow that it’s imperceptible."}
 
-class Power:
+# Special powers
+special_power = {
+    1: "Ability to read minds",
+    2: "Ability to talk to Dragons",
+    3: "I have ultra instinct level perception"}
 
-    # Dictionary with powers and descriptions
-    powers = {
-        "Sword": "Heimdall let me borrow his Sword",
-        "Knife": "I have lots of sharp knives",
-        "Flight": "Ever Seen Neo Fly. Yeah I can do that",
-        "ExtraStrength": "Think Hulk stength but no gamma radiation",
-        "Invisibility": "I’ve mastered the ability of standing so incredibly still that I become invisible to the eye. Watch. My movement’s so slow that it’s imperceptible."}
-    special_power = {
-        1: "Ability to read minds",
-        2: "Ability to talk to Dragpns",
-        3: "I have ultra instinct level perception"}
 
-    def GetPower():
-
-        power = powers[random.randint(0, len(powers))]
-        sp = special_power[random.randint(0, len(special_power))]
-        return (power, sp)
+class power():
+    def GetPower(self):
+        power = list(powers)[random.randint(0, len(powers) - 1)]
+        specialPower = special_power[list(
+            special_power)[random.randint(0, len(special_power) - 1)]]
+        return (power, specialPower)
