fd16b08fed41b8e6f92a76ee291691079b5005d4
Dan Siwiec <daniel.siwiec@gmail.com>
2019-04-03 23:54:11 -0700

    Extract story elements builders

--- game/story/story_factory.py
+++ game/story/story_factory.py
@@ -1,28 +1,19 @@
 import random
+import game.story.builders as builders
+
 
 def generate(story_length=3):
-    library = [{'type': 'monster',
-             'monster': {'name': 'Chupacabra',
-                         'strength': 5}},
+    library = [
+        builders.monster('Chupacabra', 5),
+        builders.trophy('The Sword of Damocles', 10),
+        builders.npc('Mary', 'human'),
 
+        {'type': 'mountain',
+         'mountain': {'name': 'Zagros',
+                      'greeting': 'You have arrived at Zagros mountain. The largest mountian in all of Kurdistan. Will you climb it?'}},
 
-            {'type': 'trophy',
-             'trophy': {'name': 'The Sword of Damocles',
-                        'value': 10}},
-
-            # non plyr char
-
-            {'type': 'npc',
-             'npc': {'name': 'mary',
-                     'npc_class': 'human',
-                     'greeting': 'Well hello there! My name is Mary. Can you help me??'}},
-
-            {'type': 'mountain',
-             'mountain': {'name': 'Zagros',
-                          'greeting': 'You have arrived at Zagros mountain. The largest mountian in all of Kurdistan. Will you climb it?'}},
-
-            {'type': 'cave',
-             'cave': {'greeting': 'You have arrived at the deepest darkest cave, DEATH cave. Will you enter?'}}
-            ]
+        {'type': 'cave',
+         'cave': {'greeting': 'You have arrived at the deepest darkest cave, DEATH cave. Will you enter?'}}
+    ]
 
     return random.sample(library, k=story_length)
