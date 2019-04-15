30a7a5c671005348914aad80be13012313950b28
Amarpreet Singh <amarpreet.singh1985@gmail.com>
2019-04-15 00:08:52 -0700

    added help section

--- game/__main__.py
+++ game/__main__.py
@@ -1,12 +1,15 @@
 import game.menu as menu
 import game.game as game
 import game.animations as animations
+import game.help as helper
 
 animations.show_intro()
 
 selection = menu.ask_for_selection()
 
-if selection != 'New Game':
-    print('Beep boop')
-else:
+if selection == 'New Game':
     game.start()
+elif selection == 'How to play?':
+    helper.guide()
+else:
+    print('Beep boop')
