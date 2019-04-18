37a7d440db4c48d7fde679e786a2ffc58f81c04c
Dan Siwiec <daniel.siwiec@gmail.com>
2019-04-17 18:40:12 -0700

    Extract step engine

---
+++ game/engine/step_engine.py
@@ -0,0 +1,43 @@
+import game.engine.battle as battle
+import game.npc as npc
+
+def forType(step_type):
+    if step_type == 'monster':
+        return monster
+    if step_type == 'trophy':
+        return trophy
+    if step_type == 'npc':
+        return npc
+    if step_type == 'mountain':
+        return mountain
+    if step_type == 'cave':
+        return cave
+
+def monster(step, hero):
+    monster = step['monster']
+    print(
+        f'You encounter a monster! It is a {monster["name"]} with a strength of {monster["strength"]}')
+    return battle.fight(hero, monster)
+
+def trophy(step, hero):
+    trophy = step['trophy']
+    print(
+        f'You found something! Ahhh! It is the {trophy["name"]}!')
+    return hero.pickup(trophy)
+
+def npc(step, hero):
+    npm_spec = step['npc']
+    print('You see someone in the shadows...')
+    mary = npc.npc(npm_spec['name'], npm_spec['npc_class'],
+                   npm_spec['greeting'])
+    return mary.meet()
+
+def mountain(step, hero):
+    mtn = step.get('mountain')
+    print(mtn["greeting"])
+    return battle.even_rolls()
+
+def cave(step, heo):
+    cave = step.get('cave')
+    print(cave["greeting"])
+    return battle.threeRolls()
--- game/game.py
+++ game/game.py
@@ -2,55 +2,28 @@ import game.story.welcome as welcome
 import game.story.end as end
 import game.story.story_factory as story_factory
 import game.character.creator as character_creator
-import game.npc as npc
 import game.engine.wait as wait
-import game.engine.battle as battle
+
 import game.character.character as character
 import game.progression.ability as ability
-
+import game.engine.step_engine as step_engine
 
 def play_step(step, hero):
-    print()
-    print('Lets begin ')
-    print()
     step_type = step.get('type')
-
-    if step_type == 'monster':
-        monster = step['monster']
-        print(
-            f'You encounter a monster! It is a {monster["name"]} with a strength of {monster["strength"]}')
-        return battle.fight(hero, monster)
-    if step_type == 'trophy':
-        trophy = step['trophy']
-        print(f'You found something! Ahhh! It is the {trophy["name"]}!')
-        return hero.pickup(trophy)
-    if step_type == 'npc':
-        npm_spec = step['npc']
-        print('You see someone in the shadows...')
-        mary = npc.npc(npm_spec['name'], npm_spec['npc_class'],
-                       npm_spec['greeting'])
-        return mary.meet()
-
-    if step_type == 'mountain':
-        mtn = step.get('mountain')
-        print(mtn["greeting"])
-        return battle.even_rolls()
-
-    if step_type == 'cave':
-        cave = step.get('cave')
-        print(cave["greeting"])
-        return battle.threeRolls()
-
+    return step_engine.forType(step_type)(step, hero)
 
 def start():
     welcome.show()
-
     hero = character_creator.new()
     hero.print_character()
     ability_track = ability.get_ability(hero)
 
     story = story_factory.generate()
     won = True
+    
+    print()
+    print('Lets begin ')
+    print()
     for step in story:
         wait.step_wait()
         result = play_step(step, hero)
