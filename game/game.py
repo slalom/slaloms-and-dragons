0bbda1b9698e06f628b29d8e6e013c4c3d57739b
Dan Siwiec <daniel.siwiec@gmail.com>
2019-04-17 18:43:36 -0700

    Minor cleanup

--- game/game.py
+++ game/game.py
@@ -9,8 +9,7 @@ import game.progression.ability as ability
 import game.engine.step_engine as step_engine
 
 def play_step(step, hero):
-    step_type = step.get('type')
-    return step_engine.forType(step_type)(step, hero)
+    return step_engine.forType(step.get('type'))(step, hero)
 
 def start():
     welcome.show()
@@ -21,9 +20,6 @@ def start():
     story = story_factory.generate()
     won = True
     
-    print()
-    print('Lets begin ')
-    print()
     for step in story:
         wait.step_wait()
         result = play_step(step, hero)
