62a5033d3ed064707af90f290a01524fda15df0d
Dan Siwiec <daniel.siwiec@gmail.com>
2019-05-15 18:39:55 -0700

    Add a simple map

--- game/map.py
+++ game/map.py
@@ -1,15 +1,15 @@
 import game.tools as tools
 
-
 class map:
 
-    def __init__(self, matrix, name):
-        self.matrix = matrix
-        self.name = name
+    def __init__(self, size):
+        self.model = [[" " for y in range(size)] for x in range(size)]
 
     def render(self):
-        for i in range(len(self.matrix)):
-            for j in range(len(self.matrix[i])):
-                # print(matrix[i][j])
-                print('.')
-            print()
+        print("-"*(len(self.model[0]) +2))
+        for row in self.model:
+            print("|", end="")
+            for column in row:
+                print(column, end="")
+            print("|")
+        print("-"*(len(self.model[0]) +2))
